f6f0227d9dea77eb2b8586ec4a4e09c87e16ea32
ecde4499 <ecde4499@gmail.com>
2025-10-29 12:33:42 +0900

    20251029 react

---
+++ f-iot-react/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Basic from '@/pages/a_basic';
+
+function App() {
+
+  return (
+    <>
+      <h1>안녕하세요 첫 리액트입니다. :)</h1>
+      <Basic />
+    </>
+  )
+}
+
+export default App
---
+++ f-iot-react/src/pages/a_basic/A_React.tsx
@@ -0,0 +1,21 @@
+// A_React.tsx
+
+//! rfc: 리엑트 함수형 컴포넌트 생성
+// import React from 'react'
+
+// export default function A_React() {
+//   return (
+//     <div>A_React</div>
+//   )
+// }
+
+//! rfce: 리엑트 함수형 컴포넌트 생성
+import React from 'react'
+
+function A_React() {
+  return (
+    <div>A_React</div>
+  )
+}
+
+export default A_React
