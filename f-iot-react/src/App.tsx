a4c81b6f1932b9600a339ac1fb02fd0f59590a65
ecde4499 <ecde4499@gmail.com>
2025-11-03 17:25:39 +0900

    20251103 useState / useRef

--- f-iot-react/src/App.tsx
+++ f-iot-react/src/App.tsx
@@ -1,12 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Basic from '@/pages/a_basic';
+import RoutePages from '@/pages/b_route'
+import Hooks from '@/pages/c_hooks';
+import NaviBar from './components/NaviBar';
+import PostList from './_practices/a_basic/PostList';
+import PostDetail from './components/PostDetail';
 
 function App() {
 
   return (
     <>
-      <h1>안녕하세요 첫 리액트입니다. :)</h1>
-      <Basic />
+      <h1>Korea IoT React</h1>
+      <NaviBar />
+      {/* Routes 태그: Route를 감싸는 컴포넌트 */}
+      <Routes>
+        {/* Route 태그: 특정 경로에 컴포넌트 지정 (단일 태그 권장) */}
+        <Route path='/basic' element={<Basic />} />
+        {/* 중첩 라우팅 사용을 위해서 반드시 부모 Route의 path 끝에 /*가 필수! */}
+        <Route path='/route/*' element={<RoutePages />} />
+
+        <Route path='/hooks' element={<Hooks />} />
+
+        <Route path='/practice/post' element={<PostList />} />
+        <Route path='/practice/post/:id' element={<PostDetail />} />
+
+      </Routes>
     </>
   )
 }
---
+++ f-iot-react/src/routes/A_DashBoard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function A_DashBoard() {
+  return (
+    <div>
+      <h3>대시보드 메인
+        {/*
+          Outlet 태그
+          : 중첩된 컴포넌트에서 부모 컴포넌트의 어느 위치에
+            하위 컴포넌트를 렌더링 할지 지정해주는 태그
+        */}
+        <Outlet />
+      </h3>
+    </div>
+  )
+}
+
+export default A_DashBoard
